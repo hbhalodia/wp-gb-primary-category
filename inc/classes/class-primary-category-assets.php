d7c0dd2729d141d962effb599da73cba89e4ef1b
hbhalodia <hit.bhalodia@rtcamp.com>
2022-10-13 18:08:15 +0530

    Add taxonomy support

--- inc/classes/class-primary-category-assets.php
+++ inc/classes/class-primary-category-assets.php
@@ -45,12 +45,24 @@ class WP_GB_Primary_Category_Assets {
 	public function wp_gb_primary_category_enqueue_block_editor_asset(): void {
 
 		// Enqueue Script for plugin sidebar.
-		wp_enqueue_script(
+		wp_register_script(
 			'wp-gb-primary-category-js',
 			WP_GB_PRIMARY_CATEGORY_URL . '/build/index.js',
 			self::$editor_dependency,
 			filemtime( WP_GB_PRIMARY_CATEGORY_PATH . '/build/index.js' ),
 		);
+
+		$taxonomy_name = apply_filters( 'wp_gb_primary_category_filter_taxonomy', 'category' );
+
+		wp_localize_script(
+			'wp-gb-primary-category-js',
+			'wpGbPrimaryCategory',
+			array(
+				'Taxonomy' => $taxonomy_name,
+			)
+		);
+
+		wp_enqueue_script( 'wp-gb-primary-category-js' );
 	}
 }
 new WP_GB_Primary_Category_Assets();
